9ee990765e678c5724aa29aa77833371cad02241
Jhon Cruz IMM <jhonlcp1997>
2022-05-18 16:54:33 -0500

    Probando los input

--- index.js
+++ index.js
@@ -23,8 +23,18 @@ app.get("/", (req, res)=>{
 
 // response with result but request to index.html parts
 app.post("/", (req, res)=>{
-    // let nombre = req.body.firstName;
-    // let apellido = req.body.lastName;
-    res.sendFile(__dirname + "/views/response.html");
-    // res.send(`<h2>${nombre} ${apellido}</h2>`);
+    let nombre = req.body.firstName;
+    let apellido = req.body.lastName;
+    let fecha = req.body.date;
+    let email = req.body.email;
+    let password = req.body.password;
+    let range = req.body.range;
+    // res.sendFile(__dirname + "/views/response.html");
+    res.send(`
+        <h2>${nombre} ${apellido}</h2>
+        <h2>Fecha: ${fecha}</h2>
+        <h2>email: ${email}</h2>
+        <h2>Contraseña: ${password}</h2>
+        <h2>Rango de dolor: ${range}</h2>
+    `);
 })
