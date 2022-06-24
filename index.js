7e30120a7c01aae39335a52a448dc4da8469b316
Jhon Cruz IMM <jhonlcp1997>
2022-06-24 13:33:23 -0500

    add many changes

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // require libreries only use require
 const express = require('express');
 const bodyParser = require("body-parser");
-require('dotenv').config();
-const urlimg = 'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg';
 
-// ? intento de FETCH
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-// ? intento de FETCH
+require('dotenv').config();
+
+const urlimg = 'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg';
 
 // Todo: intento correo->funcionó
 const emails = require('./static/email');
+const exportEjs = require('./static/ejs');
 
 
 // create app with express
@@ -35,8 +34,9 @@ app.listen(port, ()=>{
 
 // response route "/"
 app.get("/", (req, res)=>{
-    // res.sendFile(__dirname + "/index.html");
-    res.render('index', {titulo: "mi titulo dinámico"});
+    res.sendFile(__dirname + "/index.html");
+    console.log(__dirname);
+    // res.render('index', {titulo: "mi titulo dinámico"});
 })
 
 app.get("/services", (req, res)=>{
@@ -46,56 +46,42 @@ app.get("/services", (req, res)=>{
 
 // response with result but request to index.html parts
 app.post("/", (req, res)=>{
-    // let nombre = req.body.firstName;
-    // let apellido = req.body.lastName;
-    // let fecha = req.body.date;
-    // let email = req.body.email;
-    // let password = req.body.password;
-    // let range = req.body.range;
-    // let image = req.body.file;
+    let nombre = req.body.firstName;
+    let apellido = req.body.lastName;
+    let fecha = req.body.date;
+    let email = req.body.email;
+    let password = req.body.password;
+    let range = req.body.range;
+    let image = req.body.file;
+    let place = req.body.place;
+    let directory = __dirname;
 
-    // let fechahora =  new Date();
-    // let timestamp = fechahora.toLocaleTimeString();
+    let fechahora =  new Date();
+    let timestamp = fechahora.toLocaleTimeString();
     
-    // let props = {
-    //     nombre,
-    //     apellido,
-    //     fecha,
-    //     email,
-    //     password,
-    //     range,
-    //     timestamp
-    // }
+    let props = {
+        nombre,
+        apellido,
+        fecha,
+        email,
+        password,
+        range,
+        timestamp,
+        place,
+        directory
+    }
 
     // ? res.sendFile(__dirname + "/views/response.html");
 
-    // emails(props);
+    emails(props);
 
     // Todo: Esto es para evitar que el envio de datos por parte del node js no se complique con caracteres ñ commillas etc
     // res.setHeader('Content-Type','charset=utf-8');
     // res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
 
-    // res.send(`
-    //     <h2>Usuario: ${nombre} ${apellido}</h2>
-    //     <h2>Fecha: ${fecha}</h2>
-    //     <h2>email: ${email}</h2>
-    //     <h2>Contraseña: ${password}</h2>
-    //     <h2>Rango de dolor: ${range}</h2>
-    //     <img src="${urlimg}" />
-        // <p>TimeStamp: ${timestamp}</p>
-    // `);
-    // res.send(`
-    //     <h2>Usuario: ${nombre} ${apellido}</h2>
-    //     <h2>Fecha: ${fecha}</h2>
-    //     <h2>email: ${email}</h2>
-    //     <h2>Contraseña: ${password}</h2>
-    //     <h2>Rango de dolor: ${range}</h2>
-    //     <img src="${urlimg}" />
-    //     <p>TimeStamp: ${timestamp}</p>
-    // `);
-
     // ? intento de FETCH
-    // const data_fecth=peticion();
-    // res.send(data_fecth.id)
+
+    exportEjs(res, props);
+    
     // ? intento de FETCH
 })
